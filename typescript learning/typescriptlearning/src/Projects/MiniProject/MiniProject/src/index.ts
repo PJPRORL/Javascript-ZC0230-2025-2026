1ed83977ba4e6f24f33b9289a7dd5636ff7b54fd
jeroen piussi <piussi-jeroen@hotmail.com>
2026-05-02 15:43:21 +0200

    upload uitwerking DOM project

--- typescript learning/typescriptlearning/src/Projects/MiniProject/MiniProject/src/index.ts
+++ typescript learning/typescriptlearning/src/Projects/MiniProject/MiniProject/src/index.ts
@@ -1,7 +1,13 @@
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn")!;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
- btn?.addEventListener("click", function (){
-     alert("CLICKED!!!");
+ btn.addEventListener("click", function (){
+     alert(input.value);
+     input.value = "";
  });
 
- 
+form.addEventListener("submit", function (e){
+    e.preventDefault();
+    console.log("SUBMITTED!");
+})
