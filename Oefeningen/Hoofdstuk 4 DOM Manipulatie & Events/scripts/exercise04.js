2bc7a4fc4eccbaf3afd3e3746cb227e0d045d17b
jeroen piussi <piussi-jeroen@hotmail.com>
2026-06-04 19:47:49 +0200

    update excersise 4

--- Oefeningen/Hoofdstuk 4 DOM Manipulatie & Events/scripts/exercise04.js
+++ Oefeningen/Hoofdstuk 4 DOM Manipulatie & Events/scripts/exercise04.js
@@ -1,6 +1,11 @@
 // DOM Elementen ophalen
-const titleElements = document.querySelectorAll("title");
+const titleElements = document.getElementsByClassName("title");
 const subtitleElements = document.querySelectorAll("subtitle");
 const selectAllElements = document.querySelectorAll("title", "subtitle");
+let buttonTitle = document.getElementById("buttonTitels");
+let buttonSub = document.getElementById("buttonSub");
+let buttonAll = document.getElementById("buttonAll");
 
-titleElements.
+buttonTitle.addEventListener("click", function () {
+    titleElements.setAttribute("class", )
+})
