1def5af62d89c5a1de558f6281700eba2a0afe0c
jeroen piussi <piussi-jeroen@hotmail.com>
2026-06-06 20:16:07 +0200

    update excersise 6 from chapter 4 The DOM

--- Oefeningen/Hoofdstuk 4 DOM Manipulatie & Events/scripts/exercise06.js
+++ Oefeningen/Hoofdstuk 4 DOM Manipulatie & Events/scripts/exercise06.js
@@ -1,8 +1,24 @@
-let invoer = document.querySelector("#invoer");
+//Ophalen invoer velden
+let invoerProduct = document.querySelector("#invoerOmschrijving");
+let invoerAantal = document.querySelector("#invoerAantal")
+let invoerPrijs = document.querySelector("#invoerPrijs")
+
+//Ophalen knoppen
 let toevoegenArtikel = document.querySelector("#btnToevoegen")
+
+//Ophalen en aanmaken winkelkar
 let winkelkar = document.querySelector("#winkelkar");
 
 let lijst = [];
+
+let boodschappen = [{
+    product: invoerProduct.value,
+    aantal: invoerAantal.value,
+    prijs: invoerPrijs.value,
+    totaal: function () {
+        return this.aantal * this.prijs;
+    }
+}];
 
 function init(){
     let product = document.createElement("li");
@@ -13,16 +29,21 @@ function init(){
 window.onload = init;
 
 function producten(){
-    if (lijst.length === 0) {
+    if (boodschappen.product.length === 0) {
         winkelkar.innerHTML = "";
     }
+
+    prijs.push(invoerPrijs.value);
+    aantal.push(invoerAantal.value);
+
+    boodschappen.product.push(invoerProduct.value);
+
+    boodschappen.push(product = invoerProduct.value ,aantal = invoerAantal.value, prijs = invoerPrijs.value, this.totaal());
     
-    lijst.push(invoer.value);
-    
-    let product = document.createElement("li");
-    product.textContent = invoer.value;
-    winkelkar.appendChild(product);
-    invoer.value = "";
+    let productItem = document.createElement("li");
+    productItem.textContent = boodschappen.product.value;
+    winkelkar.appendChild(productItem);
+    boodschappen.product.value = "";
 }
 
 toevoegenArtikel.addEventListener("click", producten)
